d04efef2e08f29581de6801366766c0606583738
João Paulo Nóbrega Figueiredo <joaopnobrega@gmail.com>
2025-12-02 22:54:26 -0300

    Curso completo

---
+++ 06_SpringCloudConfigAndActuator/greeting-service/src/main/java/br/com/erudio/Startup.java
@@ -0,0 +1,16 @@
+package br.com.erudio;
+
+import br.com.erudio.config.GreetingConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties(GreetingConfiguration.class)
+public class Startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+	}
+
+}
---
+++ 19_MicroservicesRunningInKubernetesCluster/book-service/src/main/java/br/com/erudio/proxy/ExchangeProxy.java
@@ -0,0 +1,22 @@
+package br.com.erudio.proxy;
+
+import br.com.erudio.dto.Exchange;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.math.BigDecimal;
+
+// @FeignClient(name = "exchange-service")
+//@FeignClient(name = "exchange-service", url = "${EXCHANGE_SERVICE_SERVICE_HOST:http://host.docker.internal}:8000")
+@FeignClient(name = "exchange-service", url = "${EXCHANGE_SERVICE_URI:http://host.docker.internal}:8000")
+public interface ExchangeProxy {
+
+    @GetMapping(value = "/exchange-service/{amount}/{from}/{to}")
+    public Exchange getExchange(
+            @PathVariable("amount") Double amount,
+            @PathVariable("from") String from,
+            @PathVariable("to") String to);
+}
+
+//EXCHANGE_SERVICE_SERVICE_HOST
